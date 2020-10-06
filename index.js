0d372713a21280d2ba5855b7587431e546d220bf
Erin Murphy <murphy.shop66@gmail.com>
2020-10-06 19:40:14 -0400

    whoops

---
+++ index.js
@@ -0,0 +1,147 @@
+// <!--     +++++++++++ CLIP 1 AUDIO +++++++++++-->
+function play1(){
+    var audio = document.getElementById("attackOfTheManiac");
+    audio.play();
+    var display = document.getElementById("largeGif-1");
+    display.style.display ="block";
+    var display2 = document.getElementById("escape");
+    display2.style.display ="block";
+    // $('.largeGif-1').css('display', 'block')  
+    }
+   
+   
+   // <!--     +++++++++++ CLIP 2 AUDIO +++++++++++-->
+    function play2(){
+    var audio = document.getElementById("doubleAdvance");
+    audio.play();
+    var display = document.getElementById("largeGif-2");
+    display.style.display ="block";
+    var display2 = document.getElementById("escape");
+    display2.style.display ="block";
+    // $('.largeGif-1').css('display', 'block')  
+    }
+   
+   
+   // <!--     +++++++++++ CLIP 3 AUDIO +++++++++++-->
+    function play3(){
+    var audio = document.getElementById("velvetRed");
+    audio.play();
+    var display = document.getElementById("largeGif-3");
+    display.style.display ="block";
+    var display2 = document.getElementById("escape");
+    display2.style.display ="block";
+    // $('.largeGif-1').css('display', 'block')  
+    }
+   
+   
+   // <!--     +++++++++++ CLIP 4 AUDIO +++++++++++-->
+    
+   function play4(){
+            var audio = document.getElementById("sele");
+            audio.play();
+            var display = document.getElementById("largeGif-4");
+            display.style.display ="block";
+            var display2 = document.getElementById("escape");
+            display2.style.display ="block";
+            // $('.largeGif-1').css('display', 'block')  
+    }
+   
+   
+   // <!--     +++++++++++ CLIP 5 AUDIO +++++++++++-->
+   
+    function play5(){
+            var audio = document.getElementById("belleTheaterCrawl");
+            audio.play();
+            var display = document.getElementById("largeGif-5");
+            display.style.display ="block";
+            var display2 = document.getElementById("escape");
+            display2.style.display ="block";
+            // $('.largeGif-1').css('display', 'block')  
+    }
+   
+   
+   // <!--     +++++++++++ CLIP 6 AUDIO +++++++++++-->
+   
+    function play6(){
+            var audio = document.getElementById("floeMe");
+            audio.play();
+            var display = document.getElementById("largeGif-6");
+            display.style.display ="block";
+            var display2 = document.getElementById("escape");
+            display2.style.display ="block";
+            // $('.largeGif-1').css('display', 'block')  
+    }
+   
+   
+   
+    function pause(){
+    var audio = document.getElementById("attackOfTheManiac");
+    audio.pause();  
+    var display = document.getElementById("largeGif-1");
+    display.style.display ="none";
+    var display = document.getElementById("escape");
+    display.style.display ="none"; 
+      
+    var audio = document.getElementById("doubleAdvance");
+    audio.pause();  
+    var display = document.getElementById("largeGif-2");
+    display.style.display ="none";
+    var display = document.getElementById("escape");
+    display.style.display ="none";  
+      
+    var audio = document.getElementById("velvetRed");
+    audio.pause();  
+    var display = document.getElementById("largeGif-3");
+    display.style.display ="none";
+    var display = document.getElementById("escape");
+    display.style.display ="none";  
+      
+    var audio = document.getElementById("sele");
+    audio.pause();  
+    var display = document.getElementById("largeGif-4");
+    display.style.display ="none";
+    var display = document.getElementById("escape");
+    display.style.display ="none";  
+      
+    var audio = document.getElementById("belleTheaterCrawl");
+    audio.pause();  
+    var display = document.getElementById("largeGif-5");
+    display.style.display ="none";
+    var display = document.getElementById("escape");
+    display.style.display ="none"; 
+      
+    var audio = document.getElementById("floeMe");
+    audio.pause();  
+    var display = document.getElementById("largeGif-6");
+    display.style.display ="none";
+    var display = document.getElementById("escape");
+    display.style.display ="none";  
+    }
+   
+   // <!--     ++++++++++++++++++++++++++++++++++++++-->
+   
+   
+   // <!--     +++++++++++ CLIP 2 AUDIO +++++++++++-->
+   
+   
+   
+    // function pause2(){
+    // var audio = document.getElementById("notAboveLove");
+    // audio.pause();  
+    // var display = document.getElementById("largeGif-2");
+    // display.style.display ="none";
+    // var display = document.getElementById("escape");
+    // display.style.display ="none";  
+    // }
+   
+   // <!--     ++++++++++++++++++++++++++++++++++++++-->
+   
+   // if(audio == 'notAboveLove'){
+   //   audio.play();
+   // } else if(audio == 'loveSick'){
+   //   audio.blah();
+   // }
+     
+   
+   // <!--     +++++++++++ CLIP 3 AUDIO +++++++++++-->
+    
